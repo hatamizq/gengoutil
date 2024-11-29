6028b0f9d0b43be4bafcd7cf0b86b759a54e7c38
zaqi <muzaqihatami1@gmail.com>
2024-11-29 13:34:33 +0700

    feat: append slice util

--- slice.go
+++ slice.go
@@ -51,18 +51,39 @@ func SortSliceStructByKey[T any](slice []T, key string, order SortOrder) error {
 	return nil
 }
 
+// to sort slice; following with order
+func SortSlice[T any](slice []T, order SortOrder) error {
+	if len(slice) == 0 {
+		return errors.New("slice cannot be empty to sort")
+	}
+
+	sort.Slice(slice, func(i, j int) bool {
+		valI := reflect.ValueOf(slice[i])
+		valJ := reflect.ValueOf(slice[j])
+
+		return compareValues(valI, valJ, order)
+	})
+	return nil
+}
+
 // compareValues compares two reflect.Values and returns true if the first value
 // should come before the second based on the order ("asc" or "desc").
 func compareValues(fieldI, fieldJ reflect.Value, order SortOrder) bool {
 	if fieldI.Type() != fieldJ.Type() {
 		return false
 	}
+
 	switch fieldI.Kind() {
-	case reflect.Int, reflect.Int32, reflect.Int64, reflect.Float64, reflect.Float32:
+	case reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64:
 		if order == AscendingSort {
 			return fieldI.Int() < fieldJ.Int()
 		}
 		return fieldI.Int() > fieldJ.Int()
+	case reflect.Float32, reflect.Float64:
+		if order == AscendingSort {
+			return fieldI.Float() < fieldJ.Float()
+		}
+		return fieldI.Float() > fieldJ.Float()
 	case reflect.String:
 		if order == AscendingSort {
 			return fieldI.String() < fieldJ.String()
